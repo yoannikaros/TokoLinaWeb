6768bb8cbc2c90ef9f66af499426064b9c47371e
yoannikaros <yoannikaros29@gmail.com>
2022-08-08 15:22:51 +0700

    add api update

--- barang/api/connection.php
+++ barang/api/connection.php
@@ -2,7 +2,7 @@
 define ('HOST','localhost');
 define ('USER','root');
 define ('PASS','');
-define ('DB','cart_system'); 
+define ('DB','akrab_main'); 
 
 $db_connect = mysqli_connect(HOST,USER,PASS,DB) or die ('gagal koneksi bro');
 header('Content-Type: application/json');
--- barang/api/get.php
+++ barang/api/get.php
@@ -2,24 +2,22 @@
 
 require_once('connection.php');
 
-$query = "SELECT * FROM product ORDER BY id DESC";
+$query = "SELECT * FROM barang ORDER BY id DESC";
 $sql = mysqli_query($db_connect,$query);
 
 if ($sql){
     $result = array();
     while($row = mysqli_fetch_array($sql)){
         array_push($result,array(
-    'id'=>$row['id'],
-  'product_name'=>$row['product_name'],
-    'product_price'=>$row['product_price'],
-    'product_qty'=>$row['product_qty'],
-    'product_image'=>$row['product_image'],
-    'product_code'=>$row['product_code']
-
+    'kode_item'=>$row['kode_item'],
+  'barang'=>$row['barang'],
+    'jenis'=>$row['jenis'],
+    'hargaumum'=>$row['hargaumum'],
+    'hargagrosir'=>$row['hargagrosir'],
 
     ));
 
     }
-    echo json_encode (array('notes' => $result));
+    echo json_encode (array('data' => $result));
 }
 ?>
