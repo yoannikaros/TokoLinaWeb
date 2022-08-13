e434b34dea82fc73f733d310322b802a24a3a027
yoannikaros <yoannikaros29@gmail.com>
2022-08-13 14:48:46 +0700

    setting query

--- api/konsumen/hutang.php
+++ api/konsumen/hutang.php
@@ -1,7 +1,7 @@
 <?php 
 
 $connection = new mysqli("localhost","root","","akrab_main");
-$data       = mysqli_query($connection, "SELECT * FROM sales");
+$data       = mysqli_query($connection, "SELECT sum(hutang) as hutang FROM pengguna");
 $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
 
 // echo json_encode($data);
