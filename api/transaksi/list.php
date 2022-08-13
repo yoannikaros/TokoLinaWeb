d56ef26a4eff0fdc23849024121987006b9a3692
yoannikaros <yoannikaros29@gmail.com>
2022-08-13 14:50:27 +0700

    query

--- api/transaksi/list.php
+++ api/transaksi/list.php
@@ -1,7 +1,7 @@
 <?php 
 
 $connection = new mysqli("localhost","root","","akrab_main");
-$data       = mysqli_query($connection, "SELECT * FROM sales");
+$data       = mysqli_query($connection, "SELECT * FROM sales ORDER BY idsales ASC");
 $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
 
 // echo json_encode($data);
