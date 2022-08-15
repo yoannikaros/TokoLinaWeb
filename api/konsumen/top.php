accd7c0fcfbdb709090088c46252d74ab2176b31
yoannikaros <yoannikaros29@gmail.com>
2022-08-15 17:19:37 +0700

    Revisi

--- api/konsumen/top.php
+++ api/konsumen/top.php
@@ -1,7 +1,7 @@
 <?php 
 
 $connection = new mysqli("localhost","root","","akrab_main");
-$data       = mysqli_query($connection, "SELECT * FROM pengguna ORDER BY nama_pelanggan ASC");
+$data       = mysqli_query($connection, "SELECT * FROM pengguna ORDER BY Hutang ASC LIMIT 10");
 $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
 
 // echo json_encode($data);
